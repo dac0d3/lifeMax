4095d6c7869807d6fbfa27238043d0e74a22daac
Diego Carbajal <diego_carbajal3@icloud.com>
2025-03-07 13:13:04 -0800

    login sign up switch containers depending on if user has account or not

--- frontend/src/components/loginSignup/loginSignup.jsx
+++ frontend/src/components/loginSignup/loginSignup.jsx
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../css/loginSignup.css';
+import '../../assets/user.png';
+import '../../assets/letter.png';
+import '../../assets/shield.png';
 
 const LoginSignup = () => {
+    
+    const [signup,login] = useState(true);
+    
+
+    
     return (
         
-        <div class = "container">
-            <div class = "header">
-                <div class = "title">Sign Up</div>
-                <div class = "underline"></div>
+        
+// Signup container to be displayed when the user does not have an account (default)
+// It has a login button that will be used to switch to the login container
+// The signup button will be used to sign up the user into the application
+// The username, email and password fields are used to get the user's details     
+        <>
+        {signup? (
+        <div className = "signup-container">
+            <div className = "header">
+                <div className = "text">Sign Up</div>
+                <div className = "underline"></div>
             </div>
             
-            <div class = "inputs">
-                <div class = "input"></div>
-                <input type = "text" placeholder = "Username" />
-                <input type = "text" placeholder = "Email" />
-                <input type = "password" placeholder = "Password" />
+            <div className = "inputs">
+                <div className = "input">
+                    <input type = "text" placeholder = "  Username" />
+                    
+                </div>
+                <div className = "input">
+                    <input type = "email" placeholder = "  Email" />
+                </div>
+                <div className = "input">
+                    <input type = "password" placeholder = "  Password" />
+                </div>
+                
+                
             </div>
 
+            <div className = "submit-container">
+                <div className = "login" onClick = {()=>login(!signup)} >Login</div>
+                <div className = "signup" >Sign Up</div>
+            </div>
                
     
         </div>
-        
+
+        ):(
+// Login container to be displayed if login is clicked on the signup container if the user already 
+// has an account. It has a forgot password link that will be used to reset the password
+// The login button will be used to login the user into the application
+// The signup button will be used to switch to the signup container
+
+        <div className = "login-container">
+            <div className = "header">
+                <div className = "text">Login</div>
+                <div className = "underline"></div>
+            </div>
+            
+            <div className = "inputs">
+                <div className = "input">
+                    <input type = "text" placeholder = "  Username" />
+                    
+                </div>
+
+                <div className = "input">
+                    <input type = "password" placeholder = "  Password" />
+                </div>
+                
+                
+            </div>
+
+            <div className = "forgot-password">
+                Forgot Password? <span>Click here! </span>
+            </div>
+
+            <div className = "submit-container">
+                <div className = "signup" onClick = {()=>login(!signup)} >Sign Up</div>
+                <div className = "login">Login</div>
+            </div>
+        </div>
+        )}
+        </>
     );
 }
 
