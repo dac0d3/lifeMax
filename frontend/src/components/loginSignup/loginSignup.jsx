7b46880bb1e44ff9e7ef534773e975f3f909de0f
Diego Carbajal <diego_carbajal3@icloud.com>
2025-03-06 16:36:02 -0800

    updates to the user sign in

---
+++ frontend/src/components/loginSignup/loginSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../css/loginSignup.css';
+
+const LoginSignup = () => {
+    return (
+        <div class = "container">
+            <div class = "header">
+                <div class = "title">Sign Up</div>
+                <div class = "underline"></div>
+            </div>
+            
+            <div class = "inputs">
+                <div class = "input"></div>
+                <input type = "text" placeholder = "Username" />
+                <input type = "text" placeholder = "Email" />
+                <input type = "password" placeholder = "Password" />
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignup;
+
+
+
