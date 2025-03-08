c4333ec37ec711a0e2d41e093445582991b9389d
Diego Carbajal <diego_carbajal3@icloud.com>
2025-03-08 10:23:54 -0800

    added dashboard and meal calculator components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './components/loginSignup/loginSignup';
+import MealCalculator from './components/mealCalculator';
+import Dashboard from './components/dashboard';
+
 
 function App() {
   return (
     <div >
-      <LoginSignup />
-      
+      <Dashboard />
     </div>
   );
 }
---
+++ frontend/src/components/mealCalculator.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+
+const  MealCalculator = () => {
+
+    return (
+        <div>
+            <h1>Meal Calculator</h1>
+        </div>
+    )
+
+
+
+} 
+
+
+
+
+
+
+
+
+export default MealCalculator;
